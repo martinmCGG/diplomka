15637f5a9b4361632ec262ea3b077e1bcf5a8804
Alobal123 <miroslavkrabec@seznam.cz>
2018-11-29 13:36:25 +0100

    Update tf_interpolate.sh

--- src/3Dcomparison/PNET2/compile_tf_ops/tf_interpolate.sh
+++ src/3Dcomparison/PNET2/compile_tf_ops/tf_interpolate.sh
@@ -1,6 +1,4 @@
 
 TF_INC="/usr/local/lib/python2.7/dist-packages/tensorflow/include"
-echo $TF_INC
 TF_LIB="/usr/local/lib/python2.7/dist-packages/tensorflow"
-echo $TF_LIB
 g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-9.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-9.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
