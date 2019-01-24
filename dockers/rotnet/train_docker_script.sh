59c117fd2720795db5a2a41be40498d9be817f4b
Alobal123 <miroslavkrabec@seznam.cz>
2019-01-24 10:23:24 +0100

    Almost working rotnet

--- dockers/rotnet/train_docker_script.sh
+++ dockers/rotnet/train_docker_script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+docker build -t cafferotnet .
 docker kill cafferotnet
 docker rm cafferotnet
-docker run --runtime=nvidia -id --name cafferotnet -v /home/krabec/models:/home/krabec/models caffe/rotnet:latest sh
-docker exec -it cafferotnet bash -c "cd /home/krabec/models/ROTNET && /opt/caffe/caffe-rotationnet2/build/tools/caffe train -solver Training/rotationnet_modelnet40_case2_solver.prototxt -weights caffe_nets/ilsvrc13 2>&1 | tee log.txt" 
+
+dataset="/home/krabec/data/ModelNet40_mvcnn12"
+out="/home/krabec/dockers/rotnet/out"
+
+docker run --runtime=nvidia --rm -id --name cafferotnet -v "$out":/rotationnet/logs -v "$dataset":/data cafferotnet:latest sh
+#docker exec -it cafferotnet bash
+docker exec -id cafferotnet bash -c "/opt/caffe/caffe-rotationnet2/build/tools/caffe train -solver Training/rotationnet_modelnet40_case2_solver.prototxt -weights caffe_nets/ilsvrc13 -gpu 1 2>&1 | tee logs/log.txt" 
